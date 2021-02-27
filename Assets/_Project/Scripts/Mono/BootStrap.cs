8c48ee5aee371c710ffd0d758e8a668f941afe99
arnaudjopart <arnaudjopart@users.noreply.github.com>
2021-02-27 22:47:42 +0100

    Refactoring Bootstrap

--- Assets/_Project/Scripts/Mono/BootStrap.cs
+++ Assets/_Project/Scripts/Mono/BootStrap.cs
@@ -1,23 +1,25 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Entities;
 using UnityEngine;
 
-public class BootStrap : MonoBehaviour
+namespace _Project.Scripts.Mono
 {
-    private EntityManager m_entityManager;
-
-    private void Awake()
+    public class BootStrap : MonoBehaviour
     {
-        m_entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-    }
+        private EntityManager m_entityManager;
 
-    // Start is called before the first frame update
-    void Start()
-    { 
-        m_entityManager.CreateEntity(typeof(InputDataComponent));
+        private void Awake()
+        {
+            
+            m_entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            
+        }
+
+
+        void Start()
+        { 
+            
+            m_entityManager.CreateEntity(typeof(InputDataComponent));
         
+        }
     }
-    
 }
