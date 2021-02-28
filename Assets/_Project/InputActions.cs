f7d476b3ee640f55bd199d7ff4ca96433886355c
arnaudjopart <arnaudjopart@users.noreply.github.com>
2021-02-28 15:22:30 +0100

    WIP on InputSystem

---
+++ Assets/_Project/InputActions.cs
@@ -0,0 +1,205 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/_Project/InputActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+namespace com.ajc.InputSystem
+{
+    public class @InputActions : IInputActionCollection, IDisposable
+    {
+        public InputActionAsset asset { get; }
+        public @InputActions()
+        {
+            asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputActions"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""3e93520d-eb7d-4b3d-ade6-378da6a6a9c5"",
+            ""actions"": [
+                {
+                    ""name"": ""MoveAndRotate"",
+                    ""type"": ""Button"",
+                    ""id"": ""696e1e10-83a1-4c85-9398-3334b17db7d3"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""a0c2a4cd-674b-4186-8fe3-cef9af9631c9"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MoveAndRotate"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""56e80448-9610-48ac-86d1-03c1ed3460dc"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""MoveAndRotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""e3185943-a272-4a3b-8411-67e265a8e3f6"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""MoveAndRotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""852628ea-e60b-402f-91ea-e71a0f87797e"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""MoveAndRotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""99684b3b-d00c-4222-ada7-a54b629e5c54"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""MoveAndRotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard & Mouse"",
+            ""bindingGroup"": ""Keyboard & Mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+            // Gameplay
+            m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+            m_Gameplay_MoveAndRotate = m_Gameplay.FindAction("MoveAndRotate", throwIfNotFound: true);
+        }
+
+        public void Dispose()
+        {
+            UnityEngine.Object.Destroy(asset);
+        }
+
+        public InputBinding? bindingMask
+        {
+            get => asset.bindingMask;
+            set => asset.bindingMask = value;
+        }
+
+        public ReadOnlyArray<InputDevice>? devices
+        {
+            get => asset.devices;
+            set => asset.devices = value;
+        }
+
+        public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+        public bool Contains(InputAction action)
+        {
+            return asset.Contains(action);
+        }
+
+        public IEnumerator<InputAction> GetEnumerator()
+        {
+            return asset.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Enable()
+        {
+            asset.Enable();
+        }
+
+        public void Disable()
+        {
+            asset.Disable();
+        }
+
+        // Gameplay
+        private readonly InputActionMap m_Gameplay;
+        private IGameplayActions m_GameplayActionsCallbackInterface;
+        private readonly InputAction m_Gameplay_MoveAndRotate;
+        public struct GameplayActions
+        {
+            private @InputActions m_Wrapper;
+            public GameplayActions(@InputActions wrapper) { m_Wrapper = wrapper; }
+            public InputAction @MoveAndRotate => m_Wrapper.m_Gameplay_MoveAndRotate;
+            public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+            public void Enable() { Get().Enable(); }
+            public void Disable() { Get().Disable(); }
+            public bool enabled => Get().enabled;
+            public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+            public void SetCallbacks(IGameplayActions instance)
+            {
+                if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+                {
+                    @MoveAndRotate.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMoveAndRotate;
+                    @MoveAndRotate.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMoveAndRotate;
+                    @MoveAndRotate.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMoveAndRotate;
+                }
+                m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+                if (instance != null)
+                {
+                    @MoveAndRotate.started += instance.OnMoveAndRotate;
+                    @MoveAndRotate.performed += instance.OnMoveAndRotate;
+                    @MoveAndRotate.canceled += instance.OnMoveAndRotate;
+                }
+            }
+        }
+        public GameplayActions @Gameplay => new GameplayActions(this);
+        private int m_KeyboardMouseSchemeIndex = -1;
+        public InputControlScheme KeyboardMouseScheme
+        {
+            get
+            {
+                if (m_KeyboardMouseSchemeIndex == -1) m_KeyboardMouseSchemeIndex = asset.FindControlSchemeIndex("Keyboard & Mouse");
+                return asset.controlSchemes[m_KeyboardMouseSchemeIndex];
+            }
+        }
+        public interface IGameplayActions
+        {
+            void OnMoveAndRotate(InputAction.CallbackContext context);
+        }
+    }
+}
--- Assets/_Project/Scripts/Systems/InputSystem.cs
+++ Assets/_Project/Scripts/Systems/InputSystem.cs
@@ -1,3 +1,4 @@
+using com.ajc.InputSystem;
 using Unity.Entities;
 using UnityEngine;
 
@@ -5,13 +6,37 @@ namespace _Project.Scripts.Systems
 {
     public class InputSystem : SystemBase
     {
+        private InputActions m_inputActions;
+        private Vector2 m_inputVector;
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            m_inputActions = new InputActions();
+            m_inputActions.Gameplay.MoveAndRotate.performed += _context => m_inputVector = _context.ReadValue<Vector2>();
+        }
+
+        protected override void OnStartRunning()
+        {
+            base.OnStartRunning();
+            m_inputActions.Enable();
+        }
+
+        protected override void OnDestroy()
+        {
+            m_inputActions.Disable();
+        }
+
         protected override void OnUpdate()
         {
+            var vector = m_inputVector;
+            
             Entities.ForEach((ref InputComponentData _input) =>
             {
-                _input.m_inputLeft = Input.GetKey(KeyCode.Q);
-                _input.m_inputRight = Input.GetKey(KeyCode.D);
-                _input.m_inputForward = Input.GetKey(KeyCode.Z);
+                
+                _input.m_inputLeft = vector.x<0;
+                _input.m_inputRight = vector.x>0;
+                _input.m_inputForward = vector.y>0;
                 
             }).Run();
         }
