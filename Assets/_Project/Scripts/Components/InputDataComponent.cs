3b0e2fbb6a6fd4c22cd7362b692cf26e69397934
arnaudjopart <arnaudjopart@users.noreply.github.com>
2021-02-28 10:01:00 +0100

    input

---
+++ Assets/_Project/Scripts/Components/InputDataComponent.cs
@@ -0,0 +1,8 @@
+using Unity.Entities;
+
+public struct InputComponentData : IComponentData
+{
+    public bool m_inputLeft;
+    public bool m_inputRight;
+    public bool m_inputForward;
+}
---
+++ Assets/_Project/Scripts/Mono/BootStrap.cs
@@ -0,0 +1,25 @@
+using Unity.Entities;
+using UnityEngine;
+
+namespace _Project.Scripts.Mono
+{
+    public class BootStrap : MonoBehaviour
+    {
+        private EntityManager m_entityManager;
+
+        private void Awake()
+        {
+            
+            m_entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            
+        }
+
+
+        void Start()
+        { 
+            
+            m_entityManager.CreateEntity(typeof(InputComponentData));
+        
+        }
+    }
+}
