05173810d8e7e8f6df9bcd71c353e67963850647
arnaudjopart <arnaudjopart@users.noreply.github.com>
2021-03-03 23:15:17 +0100

    Wrapping System

---
+++ Assets/_Project/Scripts/Components/OffScreenDetectionSystem.cs
@@ -0,0 +1,48 @@
+
+using _Project.Scripts.Components;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class OffScreenDetectionSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var screenData = GetSingleton<ScreenDataComponent>();
+
+        Entities.WithAll<OffScreenWrapperComponent>().WithAll<MovementInfoComponent>().ForEach(
+            (Entity _entity, ref OffScreenWrapperComponent _offScreen,
+                ref MovementInfoComponent _moveComponent, ref Translation _translation) =>
+            {
+                var previousPosition = _moveComponent.m_previousPosition;
+                var currentPosition = _translation.Value;
+
+                var isMovingLeft = currentPosition.x - previousPosition.x < 0;
+                var isMovingRight = currentPosition.x - previousPosition.x > 0;
+                var isMovingUp = currentPosition.y - previousPosition.y > 0;
+                var isMovingDown = currentPosition.y - previousPosition.y < 0;
+                
+                if (_translation.Value.x < -(screenData.m_width + _offScreen.m_bounds)* .5f && isMovingLeft)
+                {
+                    //Debug.Log("Leaving screen on left");
+                    _offScreen.m_isOffScreen = true;
+                }
+                
+                if (_translation.Value.x > (screenData.m_width + _offScreen.m_bounds) * .5f && isMovingRight)
+                {
+                    //Debug.Log("Leaving screen on right");
+                    _offScreen.m_isOffScreen = true;
+                }
+                if (_translation.Value.y > (screenData.m_height + _offScreen.m_bounds) * .5f && isMovingUp)
+                {
+                    //Debug.Log("Leaving screen on top");
+                    _offScreen.m_isOffScreen = true;
+                }
+                if (_translation.Value.y < -(screenData.m_height + _offScreen.m_bounds) * .5f && isMovingDown)
+                {
+                    //Debug.Log("Leaving screen on down");
+                    _offScreen.m_isOffScreen = true;
+                }
+            }).Schedule();
+    }
+}
---
+++ Assets/_Project/Scripts/Components/ScreenWrapperComponentData_Authoring.cs
@@ -0,0 +1,25 @@
+using Unity.Entities;
+using UnityEngine;
+
+namespace _Project.Scripts.Components
+{
+    public class ScreenWrapperComponentData_Authoring : MonoBehaviour, IConvertGameObjectToEntity
+    {
+        public MeshRenderer m_mesh;
+        
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            dstManager.AddComponent(entity, typeof(OffScreenWrapperComponent));
+            dstManager.SetComponentData(entity, new OffScreenWrapperComponent
+            {
+                m_bounds = m_mesh.bounds.extents.magnitude
+            });
+        }
+    }
+
+    public struct OffScreenWrapperComponent : IComponentData
+    {
+        public float m_bounds;
+        public bool m_isOffScreen;
+    }
+}
