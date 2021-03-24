90e9b553fb609d3a4a8e31d6c55e78c6438a0314
Arnaud Jopart <arnaud.jopart@gmail.com>
2021-03-24 22:25:17 +0100

    refactoring weaponSystem

--- Assets/_Project/Scripts/Systems/WeaponSystem.cs
+++ Assets/_Project/Scripts/Systems/WeaponSystem.cs
@@ -1,7 +1,5 @@
-using System;
 using _Project.Scripts.Components;
 using Unity.Entities;
-using Unity.Entities.UniversalDelegates;
 using Unity.Transforms;
 
 namespace _Project.Scripts.Systems
@@ -9,11 +7,11 @@ namespace _Project.Scripts.Systems
     public class WeaponSystem : SystemBase
     {
         private EndSimulationEntityCommandBufferSystem m_endSimulationEntityCommandBufferSystem;
+        
         protected override void OnCreate()
         {
             base.OnCreate();
             m_endSimulationEntityCommandBufferSystem = World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-            
         }
 
         protected override void OnUpdate()
@@ -22,45 +20,41 @@ namespace _Project.Scripts.Systems
             var ecb = m_endSimulationEntityCommandBufferSystem.CreateCommandBuffer().AsParallelWriter();
 
             Entities.ForEach((Entity _entity, int entityInQueryIndex , ref WeaponComponentData _weaponComponent,
-                ref Translation _translation,
-                ref Rotation _rotation) =>
+                in Translation _translation,
+                in Rotation _rotation) =>
             {
                 _weaponComponent.m_timer += deltaTime;
-                if (_weaponComponent.m_isFiring)
+                
+                if (!_weaponComponent.m_isFiring) return;
+                if (!(_weaponComponent.m_timer > _weaponComponent.m_fireRate)) return;
+                
+                _weaponComponent.m_timer = 0;
+                var newProjectile = ecb.Instantiate(entityInQueryIndex, _weaponComponent.m_projectilePrefab);
+
+                ecb.SetComponent(entityInQueryIndex , newProjectile, new Translation
                 {
-                    if (_weaponComponent.m_timer > _weaponComponent.m_fireRate)
-                    {
-                        _weaponComponent.m_timer = 0;
-                        var newProjectile = ecb.Instantiate(entityInQueryIndex, _weaponComponent.m_projectilePrefab);
+                    Value = _translation.Value
+                });
 
-                        ecb.SetComponent(entityInQueryIndex , newProjectile, new Translation
-                        {
-                            Value = _translation.Value
-                        });
+                ecb.SetComponent(entityInQueryIndex , newProjectile, new Rotation()
+                {
+                    Value = _rotation.Value
+                });
 
-                        ecb.SetComponent(entityInQueryIndex , newProjectile, new Rotation()
-                        {
-                            Value = _rotation.Value
-                        });
-
-                        ecb.SetComponent(entityInQueryIndex , newProjectile, new MovementCommandsComponentData()
-                        {
-                            m_currentlinearCommand = 1,
-                        });
+                ecb.SetComponent(entityInQueryIndex , newProjectile, new MovementCommandsComponentData()
+                {
+                    m_currentlinearCommand = 1,
+                });
                         
-                        ecb.SetComponent(entityInQueryIndex , newProjectile, new AutoDestroyAfterSeconds()
-                        {
-                            m_lifeTime = 1
-                        });
+                ecb.SetComponent(entityInQueryIndex , newProjectile, new AutoDestroyAfterSeconds()
+                {
+                    m_lifeTime = 1
+                });
 
-                        
-                        ecb.AddComponent(entityInQueryIndex, newProjectile, new ParticleEffectLink()
-                        {
-                            value = 1
-                        });
-                    }
-
-                }
+                ecb.AddComponent(entityInQueryIndex, newProjectile, new ParticleEffectLink()
+                {
+                    value = 1
+                });
 
             }).ScheduleParallel();
             m_endSimulationEntityCommandBufferSystem.AddJobHandleForProducer(Dependency);
