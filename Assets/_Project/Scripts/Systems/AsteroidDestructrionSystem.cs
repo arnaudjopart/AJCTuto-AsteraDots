bbe8e8380d903a68553dafa6ce0eb5a55971a227
Arnaud Jopart <arnaud.jopart@gmail.com>
2021-04-24 00:07:40 +0200

    Episode is completed

---
+++ Assets/_Project/Scripts/Systems/AsteroidDestructrionSystem.cs
@@ -0,0 +1,27 @@
+using _Project.Scripts.Components;
+using Unity.Entities;
+
+namespace _Project.Scripts.Systems
+{
+    public class AsteroidDestructionSystem : SystemBase
+    {
+        private EntityManager m_entityManager;
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            m_entityManager = World.EntityManager;
+        }
+
+        protected override void OnUpdate()
+        {
+            Entities.WithoutBurst().WithStructuralChanges().WithAll<AsteroidTagComponent>().ForEach((
+                Entity _entity, 
+                in DestroyableComponentData _destroyable) =>
+            {
+                if(_destroyable.m_mustBeDestroyed) m_entityManager.DestroyEntity(_entity);
+
+            }).Run();
+        }
+    }
+}
